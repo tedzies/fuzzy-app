3bdc8998205e3deed3bfc3b9fa6c9ff60bd30d3c
tedzies <77820072+tedzies@users.noreply.github.com>
2026-07-27 12:45:29 +0000

    packages and requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
+import librosa
 
 st.title("🎈 Fuzzy App")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-
-import librosa
