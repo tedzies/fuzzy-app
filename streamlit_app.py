3c047e42e174e05d5e97e76d86003a9382fed4e2
tedzies <77820072+tedzies@users.noreply.github.com>
2026-07-27 12:33:05 +0000

    1st commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Fuzzy App")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+import librosa
