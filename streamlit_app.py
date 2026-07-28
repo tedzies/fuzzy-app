fed232a9458417efb92d227460c39f8b881bc4e0
tedzies <77820072+tedzies@users.noreply.github.com>
2026-07-28 08:06:36 +0000

    add librosa in packages

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
-import librosa
 
 st.title("🎈 Fuzzy App")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+import numpy
+import librosa
